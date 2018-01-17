a2b9f5a9057db66a805fcc1666135c2b4a762b92
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-01-17 18:42:49 +0100

    Add files via upload

---
+++ Udemy/CompleteJavaMasterclass/VariablesDataTypesOperators/FloatAndDouble/FloatAndDouble.java
@@ -0,0 +1,21 @@
+package udemy.javaclass.variables.FloatAndDouble;
+
+public class Main {
+
+    public static void main(String[] args)
+	{
+    // this is comment
+	// declaration of INT variable
+	int myIntValue = 5 / 3;
+	// declaration of FLOAT variable
+	float myFloatValue = 5f  / 3f;
+	// declaration of DOUBLE variable
+	double myDoubleValue = 5d / 3d;
+	// to type print faster - sout
+	// sout of all variables
+	System.out.println("myIntValue = " + myIntValue);
+	System.out.println("myFloatValue = " + myFloatValue);
+	// Double has 2x the precision of float
+	System.out.println("myDoubleValue = " + myDoubleValue);
+    }
+}
---
+++ Udemy/CompleteJavaMasterclass/VariablesDataTypesOperators/IntShortByteLong/IntShortByteLong.java
@@ -0,0 +1,16 @@
+package udemy.javaclass.variables.IntShortByteLong;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        byte byteValue = 10;  // 1 byte
+        short shortValue = 20; // 2 bytes
+        int intValue = 50;  // 4 bytes
+
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);  // when using Long end value with "l"
+        short shortTotal = (short) (1000 + 10 * (byteValue + shortValue + intValue));
+        System.out.println("longTotal = " + longTotal);
+        System.out.println("shortTotal = " + shortTotal);
+    }
+}
