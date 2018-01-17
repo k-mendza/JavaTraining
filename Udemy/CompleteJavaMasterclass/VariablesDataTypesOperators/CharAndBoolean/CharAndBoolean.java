18bdad7b84456e616eac25048f7e61471d7bea0f
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-01-17 20:28:41 +0100

    Add files via upload

---
+++ Udemy/CompleteJavaMasterclass/VariablesDataTypesOperators/CharAndBoolean/CharAndBoolean.java
@@ -0,0 +1,16 @@
+package Udemy.CompleteJavaMasterclass.VariablesDataTypesOperators;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+    // backslash u stands for unicode 00A9 is code for special sign
+	char myChar = '\u00A9';
+	System.out.println(myChar);
+	boolean myBoolean = true;
+	System.out.println(myBoolean);
+	// challenge find assign to char and print registered special sign
+	char registeredChar = '\u00AE';
+	System.out.println(registeredChar);
+    }
+}
