764d480d67260336bf2bf7a23e1243b0180ccd82
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-01-17 20:48:09 +0100

    Add files via upload

---
+++ Udemy/CompleteJavaMasterclass/VariablesDataTypesOperators/Operators/Operators.java
@@ -0,0 +1,21 @@
+package Udemy.CompleteJavaMasterclass.VariablesDataTypesOperators;
+
+public class Operators
+{
+
+        public static void main(String[] args)
+        {
+            // String is not a primitive type its a class
+            String myString = "This is string"; // note the double quote!
+            System.out.println(myString);
+            myString = myString + ", and this is more";
+            System.out.println("myString is equal to: " + myString);  // join of two strings
+            String number = "250.75";
+            number = number + "50.23";  // there is no addition because of type of the variables
+            System.out.println(number);
+
+            String lastString = "10";
+            lastString = lastString + 50;  // this is valid java code that will be converted into string
+            System.out.println(lastString);
+        }
+}
