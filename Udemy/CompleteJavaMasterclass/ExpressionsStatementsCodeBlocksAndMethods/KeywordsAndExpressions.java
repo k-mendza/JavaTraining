01a6f39c790eb2883de24f36395be5230a2ed576
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-01-18 19:13:57 +0100

    Add files via upload

---
+++ Udemy/CompleteJavaMasterclass/ExpressionsStatementsCodeBlocksAndMethods/KeywordsAndExpressions.java
@@ -0,0 +1,13 @@
+package Udemy.CompleteJavaMasterclass.ExpressionsStatementsCodeBlocksAndMethods;
+
+public class KeywordsAndExpressions
+{
+    public static void main(String[] args)
+    {
+	int score = 50;  // score = 50 is expression
+	if (score == 50)  // score == 50 is the expression
+    {
+        System.out.println("You have the right score!");  // "You have the right score!" is the expression
+    }
+    }
+}
