d7ee5517295f35ef6ee61635d1f9fa2b4e245fcd
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-01-18 22:27:37 +0100

    Add files via upload

---
+++ Udemy/CompleteJavaMasterclass/ExpressionsStatementsCodeBlocksAndMethods/Statements.java
@@ -0,0 +1,11 @@
+package Udemy.CompleteJavaMasterclass.ExpressionsStatementsCodeBlocksAndMethods;
+
+public class Statements {
+    public static void main(String[] args) {
+        int myInt = 50;  // myInt = 50 - expression, int myInt = 50 statement
+        myInt++;  // statement
+        myInt--;  // statement
+        System.out.println("This is expression");
+        System.out.println("This is statement: System.out.println('This is expression')");
+    }
+}
