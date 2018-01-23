0510f542d43d90b8f2f18146a41946ac7f632676
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-01-23 21:32:50 +0100

    Add files via upload

--- Udemy/CompleteJavaMasterclass/Arrays/ArrayList/GroceryList.java
+++ Udemy/CompleteJavaMasterclass/Arrays/ArrayList/GroceryList.java
@@ -1,36 +1,62 @@
-package Udemy.CompleteJavaMasterclass.Arrays.ArrayList;
+package com.timbuchalka;
 
 import java.util.ArrayList;
 
+/**
+ * Created by dev on 23/08/15.
+ */
 public class GroceryList {
-    private int[] myNumbers;
     private ArrayList<String> groceryList = new ArrayList<String>();
 
-    public void addGrroceryItem(String item){
+    public void addGroceryItem(String item) {
+
         groceryList.add(item);
     }
-    // to print items in ArrayList
-    public void printGroceryList(){
-        System.out.println("Grocery list: " +groceryList.size());
-        for (int i = 0; i<groceryList.size(); i++){
-            System.out.println(groceryList.get(i));
+
+
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for(int i=0; i< groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
         }
     }
-    // to update item in ArrayList[position]
-    public void modifyGroceryList(int position, String newItem){
-        groceryList.set(position, newItem);
+
+    public void modifyGroceryItem(String currentItem, String newItem) {
+        int position = findItem(currentItem);
+        if(position >= 0) {
+            modifyGroceryItem(position, newItem);
+        }
+
     }
-    // to remove item in ArrayList[position]
-    public void removeGroceryItem(int position){
+    private void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position+1) + " has been modified.");
+    }
+
+    public void removeGroceryItem(String item) {
+        int position = findItem(item);
+        if(position >= 0) {
+            removeGroceryItem(position);
+        }
+    }
+    private void removeGroceryItem(int position) {
         groceryList.remove(position);
     }
-    // return position of item in the ArrayList
-    public String findItem(String searchItem){
-         // boolean exists = groceryList.contains(searchItem);
-         int position = groceryList.indexOf(searchItem);
-         if (position >= 0){
-             return groceryList.get(position);
-         }
-         return null;
+
+    private int findItem(String searchItem) {
+        return groceryList.indexOf(searchItem);
+    }
+
+    public boolean onFile(String searchItem) {
+        int position = findItem(searchItem);
+        if(position >=0) {
+            return true;
+        }
+
+        return false;
     }
 }
