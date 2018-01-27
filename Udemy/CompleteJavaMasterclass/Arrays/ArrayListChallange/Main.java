842839b1acfdad66ab04feeb00b002febdaa1903
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-01-27 12:33:09 +0100

    Add files via upload

--- Udemy/CompleteJavaMasterclass/Arrays/ArrayListChallange/Main.java
+++ Udemy/CompleteJavaMasterclass/Arrays/ArrayListChallange/Main.java
@@ -8,82 +8,89 @@ public class Main {
     private static MobilePhone mobilePhone = new MobilePhone("111 222 333");
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         boolean quit = false;
         startPhone();
         printActions();
-        while (!quit){
+        while (!quit) {
             System.out.println("Enter action: ");
-            int action = s.nextInt();
+            String action = s.nextLine();
 
-            switch (action){
-                case 0:
+            switch (action) {
+                case "0":
                     System.out.println("Quitting");
                     quit = true;
                     break;
-                case 1:
+                case "1":
                     mobilePhone.printContacts();
                     break;
-                case 2:
+                case "2":
                     addNewContact();
                     break;
-                case 3:
+                case "3":
                     updateContact();
                     break;
-                case 4:
+                case "4":
                     removeContact();
                     break;
-                case 5:
+                case "5":
                     queryContact();
                     break;
-                case 6:
+                case "6":
+                    printActions();
+                    break;
+                default:
                     printActions();
                     break;
             }
         }
     }
-    private static void addNewContact(){
+
+    private static void addNewContact() {
         System.out.println("Enter new contact name: ");
         String name = s.nextLine();
         System.out.println("Enter phone number: ");
         String number = s.nextLine();
-        Contact newContact = Contact.createContact(name,number);
+        Contact newContact = Contact.createContact(name, number);
 
-        if (mobilePhone.addNewContact(newContact)){
+        if (mobilePhone.addNewContact(newContact)) {
             System.out.println("New contact added");
         } else {
             System.out.println("Error");
         }
     }
+
     private static void queryContact() {
         System.out.println("Enter name of contact to be queried: ");
         String name = s.nextLine();
         Contact existingContactRecord = mobilePhone.queryContact(name);
-        if (existingContactRecord == null){
+        if (existingContactRecord == null) {
             System.out.println("Contact not found");
             return;
         }
         System.out.println("Name: " + existingContactRecord.getName() + ", phone " + existingContactRecord.getPhoneNumber());
     }
+
     private static void removeContact() {
         System.out.println("Enter name of contact to be deleted: ");
         String name = s.nextLine();
         Contact existingContactRecord = mobilePhone.queryContact(name);
-        if (existingContactRecord == null){
+        if (existingContactRecord == null) {
             System.out.println("Contact not found");
             return;
         }
-        if (mobilePhone.removeContact(existingContactRecord)){
+        if (mobilePhone.removeContact(existingContactRecord)) {
             System.out.println("Success deleting contact");
         } else {
             System.out.println("Error deleting contact");
         }
     }
-    private static void updateContact(){
+
+    private static void updateContact() {
         System.out.println("Enter name of contact to be updated: ");
         String name = s.nextLine();
         Contact existingContactRecord = mobilePhone.queryContact(name);
-        if (existingContactRecord == null){
+        if (existingContactRecord == null) {
             System.out.println("Contact not found");
             return;
         }
@@ -92,19 +99,22 @@ public class Main {
         System.out.println("Enter new contact number ");
         String newNumber = s.nextLine();
         Contact newContact = Contact.createContact(newName, newNumber);
-        if (mobilePhone.updateContact(existingContactRecord, newContact)){
+        if (mobilePhone.updateContact(existingContactRecord, newContact)) {
             System.out.println("Success updating record");
         } else {
             System.out.println("Error updating record");
         }
     }
-    private static void printContacts(){
+
+    private static void printContacts() {
         mobilePhone.printContacts();
     }
-    private static void startPhone(){
+
+    private static void startPhone() {
         System.out.println("Starting phone ");
     }
-    private static void printActions(){
+
+    private static void printActions() {
         System.out.println("Menu: ");
         System.out.println("\t0 - turn off");
         System.out.println("\t1 - see contacts");
