2f3c23a1ba7ce1a725cbd751fb652f8bcaafb67c
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-02-02 06:06:03 +0100

    Add files via upload

--- Udemy/CompleteJavaMasterclass/Databases/TestDB/Main.java
+++ Udemy/CompleteJavaMasterclass/Databases/TestDB/Main.java
@@ -1,9 +1,6 @@
 package Udemy.CompleteJavaMasterclass.DB;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -14,8 +11,20 @@ public class Main {
         System.out.println("Connecting to Database ...");
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\DB\\testjava.db");
             Statement statement = conn.createStatement()){
+            // CREATE IF NOT EXISTS
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts " +
+                              "(name TEXT, phone INTEGER, email TEXT)");
+            // each time the code is executed this record will be added to DB with no errors
+            // IF NOT EXISTS doesn't work here!
+            statement.execute("INSERT INTO contacts (name, phone, email)" +
+                              "VALUES('Kar', 111222333, 'myemail@email.com')");
 
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+            while (results.next()){
+                System.out.println(results.getString("name") + " " + results.getInt("phone") + " " +
+                        results.getString("email"));
+            }
         // to close connection to DB and statement:
         // statement.close();
         // conn.close();
