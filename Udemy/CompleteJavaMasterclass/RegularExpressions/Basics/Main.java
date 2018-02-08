3edd3f011097d85459e82e023c530b2e8e785d96
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-02-08 19:28:36 +0100

    Add files via upload

---
+++ Udemy/CompleteJavaMasterclass/RegularExpressions/Basics/Main.java
@@ -0,0 +1,66 @@
+package Udemy.CompleteJavaMasterclass.RegularExpressions;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String string = "I am a String. Yes I am.";
+        System.out.println(string);
+        // now the regular expression:
+        String yourString = string.replaceAll("I","You");
+        System.out.println(yourString);
+
+        String alphanumeric = "abcDeeeF1234jjasdsajglkds0043jk";
+        System.out.println(alphanumeric.replaceAll(".","Y")); // "." stands for every character
+        System.out.println(alphanumeric.replaceAll("^abcDee", "YYY")); // replacement can be different length
+
+        String secondString = "abcDeeeF1234jjasdsaabcDeeejglkds0043jk";
+        System.out.println(secondString.replaceAll("^abcDee", "YYY")); // replaces only first chain because of ^
+
+        System.out.println(alphanumeric.matches("^hello"));
+        System.out.println(alphanumeric.matches("^abcDeee")); // entire String must match the parameter to return true
+        System.out.println(alphanumeric.matches("abcDeeeF1234jjasdsajglkds0043jk"));
+
+        System.out.println(alphanumeric.replaceAll("0043jk$","The END"));
+        System.out.println(alphanumeric.replaceAll("[abce]", "X"));
+        System.out.println(alphanumeric.replaceAll("[j][a]","X"));
+
+        System.out.println("harry".replaceAll("[Hh]arry", "Harry"));
+        System.out.println("Harry".replaceAll("[Hh]arry", "harry"));
+        System.out.println(alphanumeric.replaceAll("[^ej]","X"));
+        System.out.println(alphanumeric.replaceAll("[abcdef345678]", "X"));
+        System.out.println(alphanumeric.replaceAll("[a-fA-F3-8]", "X")); // specifying ranges to obtain same results
+        System.out.println(alphanumeric.replaceAll("(?i)[a-f3-8]", "X")); // special modifier to omit letter size
+        System.out.println(alphanumeric.replaceAll("[0-9]", "X"));
+
+        String hasWhitespace = "I have blanks and\ta tab, and also new line\n";
+        System.out.println(hasWhitespace);
+        System.out.println(hasWhitespace.replaceAll("\\s", ""));
+        System.out.println(hasWhitespace.replaceAll("\t", "X"));
+        System.out.println(hasWhitespace.replaceAll("\\S",""));
+        System.out.println(hasWhitespace.replaceAll("\\w","X"));
+        System.out.println(hasWhitespace.replaceAll("\\W","X"));
+
+        String a = "abcDeefghhiiiiiijklmnoprstuwxyz1234567890";
+        System.out.println(a.replaceAll("^abcDe{3}", "YYY"));
+        System.out.println(a.replaceAll("^abcDe+", "YYY"));
+        System.out.println(a.replaceAll("abcDe*", "YYY")); // replace regardless of "e"
+        System.out.println(a.replaceAll("^abcDe{2,5}", "XXX")); // replace with X if there between 2 to 5
+        System.out.println(a.replaceAll("h+i*j","Y"));
+
+        StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
+        htmlText.append("<h2>Sub-heading</h2>");
+        htmlText.append("<p>This is a paragraph</p>");
+        htmlText.append("<p>This is another paragraph</p>");
+        htmlText.append("<h2>Summary</h2>");
+        htmlText.append("<p>Here is the summary</p>");
+
+        String h2Pattern = ".*<h2>.*"; // . means any character and * means any number
+        Pattern pattern = Pattern.compile(h2Pattern);
+        Matcher matcher = pattern.matcher(htmlText);
+        System.out.println(matcher.matches());
+    }
+}
