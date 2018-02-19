48e255c1f7148643c244239d3f8affdd042a6b1c
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-02-19 20:16:23 +0100

    Add files via upload

--- Udemy/CompleteJavaMasterclass/UnitTests/UTIntro/BankAccountTest.java
+++ Udemy/CompleteJavaMasterclass/UnitTests/UTIntro/BankAccountTest.java
@@ -6,7 +6,9 @@ class BankAccountTest {
 
     @org.junit.jupiter.api.Test
     void deposit() {
-        fail("This test has to be implemented");
+        BankAccount account = new BankAccount("Kar", "Men", 1000.0, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance); // compare account balance with expected value an tolerance 0
     }
 
     @org.junit.jupiter.api.Test
@@ -15,8 +17,27 @@ class BankAccountTest {
     }
 
     @org.junit.jupiter.api.Test
-    void getBalance() {
-        fail("This test has to be implemented");
+    void getBalance_deposit() {
+        BankAccount account = new BankAccount("Kar", "Men", 1000.0, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance());
     }
 
+    @org.junit.jupiter.api.Test
+    void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Kar", "Men", 1000.0, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void isCheckingTrue(){
+        BankAccount account = new BankAccount("Kar", "Men", 1000.0, BankAccount.CHECKING);
+        assertTrue(account.isChecking());
+    }
+    @org.junit.jupiter.api.Test
+    public void isCheckingFalse(){
+        BankAccount account = new BankAccount("Kar", "Men", 1000.0, BankAccount.SAVINGS);
+        assertFalse(account.isChecking());
+    }
 }
