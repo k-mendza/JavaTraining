a8065df1ed58f236f5448feec7666892a8f426bf
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-02-19 07:24:25 +0100

    Add files via upload

---
+++ Udemy/CompleteJavaMasterclass/UnitTests/UTIntro/BankAccountTest.java
@@ -0,0 +1,22 @@
+package Udemy.CompleteJavaMasterclass.UnitTests.Banking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        fail("This test has to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+        fail("This test has to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+        fail("This test has to be implemented");
+    }
+
+}
