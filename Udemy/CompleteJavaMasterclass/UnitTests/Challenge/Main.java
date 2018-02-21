ae657864d190c56f03eadaab55ecd694ed17a519
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-02-21 07:00:40 +0100

    Add files via upload

---
+++ Udemy/CompleteJavaMasterclass/UnitTests/Challenge/Main.java
@@ -0,0 +1,9 @@
+package Udemy.CompleteJavaMasterclass.UnitTests.Challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Utilities util = new Utilities();
+	util.removePairs("AABBCCDDEEFF");
+    }
+}
