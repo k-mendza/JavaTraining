3e52af27d8d826a60985a81f39b4bfd1779e1d9b
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-02-22 06:55:33 +0100

    Add files via upload

---
+++ Udemy/CompleteJavaMasterclass/Packages/Intro/Main.java
@@ -0,0 +1,12 @@
+package udemy.completeJavaMasterclass.packages;
+
+
+import udemy.completeJavaMasterclass.packages.MyWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Programmer");
+        myWindow.setVisible(true);
+    }
+}
