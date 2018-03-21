c10d7bbe444da27eeeb7902e0cfaea075e2eefc0
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-03-21 21:10:58 +0100

    Add files via upload

--- Udemy/CompleteJavaMasterclass/Collections/SortedCollections/Basket.java
+++ Udemy/CompleteJavaMasterclass/Collections/SortedCollections/Basket.java
@@ -28,12 +28,12 @@ public class Basket {
 
     @Override
     public String toString() {
-        String s = "\nShopping basket " + name + " contains " + list.size() + " items\n";
+        String s = "\nShopping basket " + name + " contains "  + list.size() + (list.size()  == 1 ? "item" : " items") + "\n";
         double totalCost = 0.0;
         for (Map.Entry<StockItem, Integer> item : list.entrySet()){
             s = s + item.getKey() + ", " + item.getValue() + " purchased\n";
             totalCost += item.getKey().getPrice() * item.getValue();
         }
-        return s + " Total cost " + totalCost;
+        return s + "Total cost " + totalCost;
     }
 }
--- Udemy/CompleteJavaMasterclass/Collections/SortedCollections/Main.java
+++ Udemy/CompleteJavaMasterclass/Collections/SortedCollections/Main.java
@@ -13,7 +13,9 @@ public class Main {
         stockList.addStock(temp);
         temp = new StockItem("Chair", 62.00, 10);
         stockList.addStock(temp);
-        temp = new StockItem("Bup", 0.50, 200);
+        temp = new StockItem("Cup", 0.50, 200);
+        stockList.addStock(temp);
+        temp = new StockItem("Cup", 0.47, 7);
         stockList.addStock(temp);
         temp = new StockItem("Door", 72.95, 4);
         stockList.addStock(temp);
@@ -27,5 +29,26 @@ public class Main {
         for (String s: stockList.Items().keySet()){
             System.out.println(s);
         }
+
+        Basket karBasket = new Basket("Kar");
+        sellItem(karBasket, "Car", 1);
+        System.out.println(karBasket);
+        sellItem(karBasket, "Car", 1);
+        System.out.println(karBasket);
+        sellItem(karBasket, "Car", 1);
+        sellItem(karBasket, "Spanner", 1);
+        System.out.println(karBasket);
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity){
+        StockItem stockItem = stockList.get(item);
+        if (stockItem == null){
+            System.out.println("We don't sell " + item);
+        }
+        if (stockList.sellStock(item, quantity) != 0){
+            basket.addToBasket(stockItem, quantity);
+            return quantity;
+        }
+        return 0;
     }
 }
