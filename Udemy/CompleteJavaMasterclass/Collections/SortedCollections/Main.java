399cfb61e8527549e97697a8a2b159e04468f5ea
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-03-23 21:17:12 +0100

    Add files via upload

--- Udemy/CompleteJavaMasterclass/Collections/SortedCollections/Main.java
+++ Udemy/CompleteJavaMasterclass/Collections/SortedCollections/Main.java
@@ -41,25 +41,41 @@ public class Main {
         if (sellItem(karBasket, "Car", 1) != 1){
             System.out.println("There are no more cars in stock");
         }
+
         sellItem(karBasket, "Spanner", 1);
-        System.out.println(karBasket);
 
         sellItem(karBasket, "Juice", 4);
         sellItem(karBasket, "Cup", 12);
         sellItem(karBasket, "Phone", 1);
+
+        Basket basket = new Basket("customer");
+        sellItem(basket, "Cup", 100);
+        sellItem(basket, "Juice", 5);
+        removeItem(basket, "Cup", 1);
+        System.out.println(basket);
+
+        removeItem(karBasket, "Car", 1);
+        removeItem(karBasket, "Cup", 9);
+        removeItem(karBasket, "Car", 1);
+        System.out.println("Cars removed: " + removeItem(karBasket, "Car", 1));
         System.out.println(karBasket);
-        System.out.println(stockList);
 
 //        temp = new StockItem("Pen", 1.12, 1);
 //        stockList.Items().put(temp.getName(), temp);
         stockList.Items().get("Car").adjustStock(2000);
-        stockList.Items().get("Car").adjustStock(-1000);
+        stockList.get("Car").adjustStock(-1000);
         System.out.println(stockList);
 
         for (Map.Entry<String, Double> price : stockList.PriceList().entrySet()){
             System.out.println(price.getKey() + " costs " + price.getValue());
         }
 
+        System.out.println(karBasket);
+        System.out.println(basket);
+        checkOut(karBasket);
+        checkOut(basket);
+        System.out.println(karBasket);
+        System.out.println(basket);
     }
 
     public static int sellItem(Basket basket, String item, int quantity){
@@ -85,7 +101,7 @@ public class Main {
         return 0;
     }
 
-    public static void ckeckOut(Basket basket){
+    public static void checkOut(Basket basket){
         for (Map.Entry<StockItem, Integer> item : basket.Items().entrySet()){
             stockList.sellStock(item.getKey().getName(), item.getValue());
         }
