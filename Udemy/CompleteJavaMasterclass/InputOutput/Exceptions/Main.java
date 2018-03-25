d2a601a242f9326c06efe1574c8af003045e24f2
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-03-26 00:06:34 +0200

    Add files via upload

--- Udemy/CompleteJavaMasterclass/InputOutput/Exceptions/Main.java
+++ Udemy/CompleteJavaMasterclass/InputOutput/Exceptions/Main.java
@@ -1,5 +1,7 @@
 package udemy.completeJavaMasterclass;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,11 +11,47 @@ public class Main {
          * two different approaches of handling exceptions presented below
          */
 
-        int x = 98;
-        int y = 0;
-        System.out.println(divideLBYL(x, y)); // works ok
-        System.out.println(divideEAFP(x, y)); // works ok
-        System.out.println(divide(x, y)); // crashes
+//        int x = 98;
+//        int y = 0;
+//        System.out.println(divideLBYL(x, y)); // works ok
+//        System.out.println(divideEAFP(x, y)); // works ok
+//        System.out.println(divide(x, y)); // crashes
+        int x = getIntEAFP();
+        System.out.println("X = " + x);
+    }
+
+    private static int getInt(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter value of X:");
+        return scanner.nextInt();
+    }
+
+    private static int getIntEAFP(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter value of integer: ");
+        try {
+            return scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println(e);
+            return 0;
+        }
+    }
+
+    private static int getIntLBYL(){
+        Scanner scanner = new Scanner(System.in);
+        boolean valid = true;
+        System.out.println("Please enter value of integer: ");
+        String input = scanner.next();
+        for (int i=0; i<input.length(); i++){
+            if (!Character.isDigit(input.charAt(i))){
+                valid = false;
+                break;
+            }
+        }
+        if (valid){
+            return Integer.parseInt(input);
+        }
+        return 0;
     }
 
     private static int divideLBYL(int x, int y){
@@ -33,7 +71,7 @@ public class Main {
         }
     }
 
-    public static int divide(int x, int y){
+    private static int divide(int x, int y){
         return x/y;
     }
 }
