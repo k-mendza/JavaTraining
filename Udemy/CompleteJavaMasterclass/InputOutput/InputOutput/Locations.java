1367ff44865248007674603ae9b4a6edd1eff62f
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-03-29 23:23:21 +0200

    Add files via upload

--- Udemy/CompleteJavaMasterclass/InputOutput/InputOutput/Locations.java
+++ Udemy/CompleteJavaMasterclass/InputOutput/InputOutput/Locations.java
@@ -1,5 +1,6 @@
 package udemy.completeJavaMasterclass;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -58,6 +59,33 @@ public class Locations implements Map<Integer, Location> {
                 scanner.close();
             }
         }
+
+
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader("directions.txt")));
+            scanner.useDelimiter(", ");
+            while (scanner.hasNextLine()) {
+//                int loc = scanner.nextInt();
+//                String direction = scanner.next();
+//                scanner.skip(scanner.delimiter());
+//                String dest = scanner.nextLine();
+//                int destination = Integer.parseInt(dest);
+                String input = scanner.nextLine();
+                String[] data = input.split(", ");
+                int loc = Integer.parseInt(data[0]);
+                String direction = data[1];
+                int destination = Integer.parseInt(data[2]);
+                System.out.println(loc + ": " + direction + ": " + destination);
+                Location location = locations.get(loc);
+                location.addExit(direction, destination);
+            }
+        } catch (IOException e){
+           e.printStackTrace();
+        } finally {
+            if (scanner != null){
+                scanner.close();
+            }
+    }
 //        Map<String, Integer> tempExit = new HashMap<String, Integer>();
 //        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",null));
 //
