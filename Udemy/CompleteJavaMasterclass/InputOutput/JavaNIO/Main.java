42a28344ef1710cde15cd4d1aa32ec8aa88b176e
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-04-08 23:34:07 +0200

    Add files via upload

--- Udemy/CompleteJavaMasterclass/InputOutput/JavaNIO/Main.java
+++ Udemy/CompleteJavaMasterclass/InputOutput/JavaNIO/Main.java
@@ -3,6 +3,7 @@ package com.karmen;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
@@ -26,22 +27,53 @@ public class Main {
             intBuffer.flip();
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes = " + numBytes);
+
             intBuffer.flip(); // first flip to reset buffer position
             intBuffer.putInt(-987654321);
             intBuffer.flip(); // second flip to reset buffer position
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes = " + numBytes);
 
+            RandomAccessFile randomAccessFile = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = randomAccessFile.getChannel();
+            outputBytes[0] = 'a';
+            outputBytes[1] = 'b';
+            buffer.flip();
+            long numBytesRead = channel.read(buffer);
+            if (buffer.hasArray()) {
+                System.out.println("byte buffer = " + new String(buffer.array()));
+            }
 
-////            FileInputStream file = new FileInputStream("data.txt");
-////            FileChannel channel = file.getChannel();
-//            Path dataPath = FileSystems.getDefault().getPath("data.txt");
-//            // to write data into file:
-//            Files.write(dataPath, "\nLine 6".getBytes("UTF-8"),StandardOpenOption.APPEND);
-//            List<String> lines = Files.readAllLines(dataPath);
-//            for (String line : lines) {
-//                System.out.println(line);
-//            }
+            // Absolute read
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+
+            // Relative read
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//            intBuffer.flip();
+//            System.out.println(intBuffer.getInt());
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//            intBuffer.flip();
+//            System.out.println(intBuffer.getInt());
+//            channel.close();
+//            randomAccessFile.close();
+
+//            RandomAccessFile randomAccessFile = new RandomAccessFile("data.dat", "rwd");
+//            byte[] bytes = new byte[outputBytes.length];
+//            randomAccessFile.read(bytes);
+//            System.out.println(new String(bytes));
+//
+//            // reading integers
+//            long int1 = randomAccessFile.readInt();
+//            long int2 = randomAccessFile.readInt();
+//            System.out.println(int1);
+//            System.out.println(int2);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
