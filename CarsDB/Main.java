d74caf56f7e197618144c9486199b90e4d4566e5
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-04-15 22:38:22 +0200

    Add files via upload

--- CarsDB/Main.java
+++ CarsDB/Main.java
@@ -1,6 +1,10 @@
 package com.karmen;
 
+import com.karmen.Model.Car;
+import com.karmen.Model.CarsArrayList;
+
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * It is required to provide sqlite.jar driver for JDBC to be able to read data from DB.
@@ -14,9 +18,14 @@ public class Main {
         try(Connection conn = DriverManager.getConnection(CONNECTION_STRING)){
             Statement statement = conn.createStatement();
             ResultSet resultSet = statement.executeQuery("select Brands.BrandName, Models.ModelName, Models.Price from Brands inner join Models ON Brands.BrandID = Models.BrandID");
+
+            ArrayList<Car> carList = new ArrayList<>();
             while(resultSet.next()) {
-                System.out.println(resultSet.getString("BrandName") + " " + resultSet.getString("ModelName") + " " + resultSet.getDouble("Price"));
+                //System.out.println(resultSet.getString("BrandName") + " " + resultSet.getString("ModelName") + " " + resultSet.getDouble("Price"));
+                carList.add(new Car(resultSet.getString("BrandName"),resultSet.getString("ModelName"),resultSet.getDouble("Price")));
             }
+            CarsArrayList carsArrayList = new CarsArrayList(carList);
+            carsArrayList.printCarsArrayList();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
