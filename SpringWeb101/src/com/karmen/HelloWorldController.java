4b12a96595c457788d4e91925912ce91ad1aad34
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-04-22 19:25:49 +0200

    Add files via upload

--- SpringWeb101/src/com/karmen/HelloWorldController.java
+++ SpringWeb101/src/com/karmen/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.karmen;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -16,4 +20,20 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    @RequestMapping("/processFormVersionTwo")
+    public String processFormVersionTwo(HttpServletRequest request, Model model){
+        String name = request.getParameter("studentName");
+        name = name.toUpperCase();
+        String result = "Yo! " + name;
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String name, Model model){
+        name = name.toUpperCase();
+        String result = "Hey " + name + " from method version 3!";
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
 }
