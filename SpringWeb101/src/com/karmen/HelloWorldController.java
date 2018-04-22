2a62d45b34b92a2ce79e2038a2c66f565878783e
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-04-22 19:40:20 +0200

    Add files via upload

--- SpringWeb101/src/com/karmen/HelloWorldController.java
+++ SpringWeb101/src/com/karmen/HelloWorldController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/hello") // parent request mapping = all of the method's adresses will start from /hello
 public class HelloWorldController {
 
     @RequestMapping("/showForm")
