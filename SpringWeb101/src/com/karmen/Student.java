58e6170713ac5e64a10913399870ae6a3f2d5d9d
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-04-23 22:30:43 +0200

    Add files via upload

--- SpringWeb101/src/com/karmen/Student.java
+++ SpringWeb101/src/com/karmen/Student.java
@@ -2,23 +2,33 @@ package com.karmen;
 
 public class Student {
     private String firstName;
-    private String LastName;
+    private String lastName;
+    private String country;
 
-    public Student(){}
+    public Student() {
+    }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void setLastNamel(String lastNamel) {
-        LastName = lastNamel;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public String getLastNamel() {
-        return LastName;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCountry() {
+        return country;
     }
 }
