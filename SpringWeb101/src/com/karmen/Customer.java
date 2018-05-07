2ac5f3fd09be37a7fafcaaf6ee28609891c01cde
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-05-07 23:11:39 +0200

    Add files via upload

--- SpringWeb101/src/com/karmen/Customer.java
+++ SpringWeb101/src/com/karmen/Customer.java
@@ -1,9 +1,6 @@
 package com.karmen;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Customer {
 
@@ -18,6 +15,9 @@ public class Customer {
     @Min(value=0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
     private int freePasses;
+
+    @Pattern(regexp="^[a-zA-Z0-9]{5}", message = "only 5 characters/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return this.firstName;
@@ -41,5 +41,13 @@ public class Customer {
 
     public void setFreePasses(int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
