f2546b27454fd4042684ef915bcbf3280c4aa949
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-05-18 23:48:57 +0200

    Add files via upload

--- hibernate101/src/main/java/demo/CreateStudentDemo.java
+++ hibernate101/src/main/java/demo/CreateStudentDemo.java
@@ -8,20 +8,46 @@ import org.hibernate.cfg.Configuration;
 public class CreateStudentDemo {
 
     public static void main(String[] args) {
+//        Student myStudent = new Student("Carlos", "Santana", "carlos@test.com");
+//        insertStudentIntoDB(myStudent);
+        System.out.println(getStudentFromDB(1));
+    }
+
+    private static void insertStudentIntoDB(Student student){
         SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Student.class)
                 .buildSessionFactory();
 
         Session session = sessionFactory.getCurrentSession();
+
         try {
-            Student newStudent = new Student("Paul", "Wall", "paul@wall.com");
             session.beginTransaction();
-            session.save(newStudent);
+            session.save(student);
             session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             sessionFactory.close();
         }
+    }
+
+    private static Student getStudentFromDB(int studentId){
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+        Student student = new Student();
+        try {
+            session.beginTransaction();
+            student = session.get(Student.class, studentId);
+            session.getTransaction().commit();
+            return student;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sessionFactory.close();
+        }
+        return student;
     }
 }
