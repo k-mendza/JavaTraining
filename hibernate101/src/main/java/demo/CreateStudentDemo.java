70b6a25efed334a94c253623492627f402b0dd7c
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-05-19 00:36:54 +0200

    Add files via upload

--- hibernate101/src/main/java/demo/CreateStudentDemo.java
+++ hibernate101/src/main/java/demo/CreateStudentDemo.java
@@ -5,12 +5,19 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateStudentDemo {
 
     public static void main(String[] args) {
 //        Student myStudent = new Student("Carlos", "Santana", "carlos@test.com");
 //        insertStudentIntoDB(myStudent);
-        System.out.println(getStudentFromDB(1));
+        List<Student> allStudents = getAllStudentsFromDB();
+
+        for (Object obj : allStudents) {
+            System.out.println(obj);
+        }
     }
 
     private static void insertStudentIntoDB(Student student){
@@ -42,12 +49,30 @@ public class CreateStudentDemo {
             session.beginTransaction();
             student = session.get(Student.class, studentId);
             session.getTransaction().commit();
-            return student;
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             sessionFactory.close();
         }
         return student;
+    }
+
+    private static List<Student> getAllStudentsFromDB(){
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+        List<Student> studentsList = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            studentsList = session.createQuery("from Student").getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sessionFactory.close();
+        }
+        return studentsList;
     }
 }
